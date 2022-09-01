5a4820e59f3e9559a37bc9e8414f52e1bec3fcff
DIGITEACHBOX29041209 <lina.fernanda.rc@gmail.com>
2022-09-01 12:50:16 -0500

    ajuste

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ app.use(bodyparser.json())
 
 // Se inicializa el WebHook para que siempre este escuchando en el puerto especificado en la linea 4, validar regla de entrada en el firewall (debe estar creada con el mismo puerto)
 app.listen(port, () => {
-    console.log('Webhook corriendo en el puerto ' + port)
+    console.log('Webhook corriendo en el puerto ' + port + "\n")
 })
 
 // Cada vez que realicen una peticion al WebHook con el path /webhookagro Iniciara este proceso
 app.get('/webhookagro', (req, res) => {
-    console.log('Bienvenido al WebHook-AgroApoya2')
+    console.log('Bienvenido al WebHook-AgroApoya2 \n')
         //Recuperamos valores del body enviados por el tercero que llamo al webhook
     var body = req.body;
     var bodystring = JSON.stringify(body);
@@ -33,20 +33,20 @@ app.get('/webhookagro', (req, res) => {
         }
     }).then(ValConCorrecta => {
         //En caso de que se logre la conexion se realiza el siguiente proceso
-        console.log('Conexión BD creada')
+        console.log('Conexión BD creada \n')
         const Insercion = sql.query("Insert into Trans_Pago_Agro values('" + body.id + "','" + body.ammount + "','" + body.externalorder + "','" + body.ip + "','" + body.fullname + "','" + body.additionaldata + "'" +
             ",'" + status.id + "','" + status.nombre + "','" + persona.id + "','" + persona.firstname + "','" + persona.lastname + "','" + persona.identification + "','" + persona.email + "','" + persona.phone + "','" + body.paymentmethod + "','" + body.nombre + "','" + bodystring + "')").then(valInsert => {
             //Posterior al realizar la insercion, si esta se realizo correctamente cerramos nuestra conexion a la BD, esto para evitar hilos no controlados
             ValConCorrecta.close();
-            console.log('Transacción realizada')
-            console.log('Conexión BD finalizada')
+            console.log('Transacción realizada \n')
+            console.log('Conexión BD finalizada \n')
                 //Retornamos respuesta de proceso realizado correctamente
             res.send({
                 resultado: 'Transacción realizada'
             })
         }, ValNoInsert => {
             //En caso de presentarse error en la insercion se realiza el siguiente proceso
-            console.log('Transacción no insertada')
+            console.log('Transacción no insertada \n')
             console.log(ValNoInsert)
                 //Igualmente retornamos respuesta para finalizar el hilo, si no finalizamos quedara siempre EN PROCESO y se toteara por timeout afectando al servidor
             res.send({
